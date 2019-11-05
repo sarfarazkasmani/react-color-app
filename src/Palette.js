11d4b7db7f44b3a583c29fb54f5a96067a2667d2
sarfarazkasmani <sarfarazkasmani@gmail.com>
2019-11-05 16:35:28 +0530

    styling done for slider

--- src/Palette.js
+++ src/Palette.js
@@ -1,39 +1,45 @@
-import React, { Component } from 'react'
-import ColorBox from './ColorBox'
-import './Palette.css'
-import Slider from 'rc-slider';
+import React, { Component } from 'react';
+import ColorBox from './ColorBox';
 import 'rc-slider/assets/index.css';
+import './Palette.css';
+import Slider from 'rc-slider';
 
 class Palette extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            level: 500
-        }
-        this.changeLevel = this.changeLevel.bind(this)
-    }
-    changeLevel(level) {
-        this.setState({
-            level: level
-        })
-    }
-    render() {
-        const { colors } = this.props.palette;
-        const { level } = this.state
-        const colorBoxes = colors[level].map(color => (
-            <ColorBox background={color.hex} name={color.name} />
-        ))
-        return (
-            <div className="Palette" >
-                <Slider defaultValue={level} step={100} min={100} max={900} onAfterChange={this.changeLevel} />
-                {/* //TODO: Navbar goes here */}
-                <div className="Palette-colors">
-                    {colorBoxes}
-                </div>
-                {/* //TODO: footer  */}
-            </div>
-        )
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			level: 500
+		};
+		this.changeLevel = this.changeLevel.bind(this);
+	}
+	changeLevel(level) {
+		this.setState({
+			level: level
+		});
+	}
+	render() {
+		const { colors } = this.props.palette;
+		const { level } = this.state;
+		const colorBoxes = colors[level].map((color) => (
+			<ColorBox background={color.hex} name={color.name} />
+		));
+		return (
+			<div className="Palette">
+				<div className="slider">
+					<Slider
+						defaultValue={level}
+						step={100}
+						min={100}
+						max={900}
+						onAfterChange={this.changeLevel}
+					/>
+				</div>
+				{/* //TODO: Navbar goes here */}
+				<div className="Palette-colors">{colorBoxes}</div>
+				{/* //TODO: footer  */}
+			</div>
+		);
+	}
 }
 
-export default Palette
+export default Palette;
