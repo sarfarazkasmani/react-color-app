3a004af630aa7e6462cb111c30f0314f34873016
sarfarazkasmani <sarfarazkasmani@gmail.com>
2019-11-07 14:56:15 +0530

    Fixed palette route

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 import React, { Component } from 'react';
-import {Route, Switch} from 'react-router-dom'
-import Palette from './Palette'
-import seedColors from './seedColors'
-import { generatePalette } from './colorHelpers'
+import { Route, Switch } from 'react-router-dom';
+import Palette from './Palette';
+import seedColors from './seedColors';
+import { generatePalette } from './colorHelpers';
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <Switch>
-          <Route exact path="/" render={() => <h1>Palette list goes here.</h1>} />
-          <Route exact path="/palette/:id" render={() => <h1>Individual palette</h1>} />
-        </Switch>
-        {/* <div>
+	findPalette(id) {
+		return seedColors.find(function(palette) {
+			return palette.id === id;
+		});
+	}
+	render() {
+		return (
+			<div>
+				<Switch>
+					<Route exact path="/" render={() => <h1>Palette list goes here.</h1>} />
+					<Route
+						exact
+						path="/palette/:id"
+						render={(routeProps) => (
+							<Palette
+								palette={generatePalette(
+									this.findPalette(routeProps.match.params.id)
+								)}
+							/>
+						)}
+					/>
+				</Switch>
+				{/* <div>
           <Palette palette={generatePalette(seedColors[4])} />
         </div> */}
-      </div>
-    );
-  }
+			</div>
+		);
+	}
 }
 
 export default App;
