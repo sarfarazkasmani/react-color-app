a3f9638802460d17359f77e196a1e248fd973d41
Sarfaraz Kasmani <sarfarazkasmani@Sarfarazs-MacBook-Air.local>
2019-11-09 20:58:27 +0530

    add link to colorbox and add route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Palette from './Palette';
-import PaletteList from './PaletteList'
+import PaletteList from './PaletteList';
 import seedColors from './seedColors';
 import { generatePalette } from './colorHelpers';
 
@@ -15,22 +15,26 @@ class App extends Component {
 		return (
 			<div>
 				<Switch>
-					<Route exact path="/" render={(routeProps) => <PaletteList palettes={seedColors} {...routeProps} />} />
+					<Route
+						exact
+						path="/"
+						render={(routeProps) => <PaletteList palettes={seedColors} {...routeProps} />}
+					/>
 					<Route
 						exact
 						path="/palette/:id"
 						render={(routeProps) => (
 							<Palette
-								palette={generatePalette(
-									this.findPalette(routeProps.match.params.id)
-								)}
+								palette={generatePalette(this.findPalette(routeProps.match.params.id))}
 							/>
 						)}
 					/>
+					<Route
+						exact
+						path="/palette/:paletteId/:colorId"
+						render={() => <h1>Single Color Page</h1>}
+					/>
 				</Switch>
-				{/* <div>
-          <Palette palette={generatePalette(seedColors[4])} />
-        </div> */}
 			</div>
 		);
 	}
