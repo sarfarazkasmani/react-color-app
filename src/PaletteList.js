1683464398672a432687d301d3842d8b094d5007
sarfarazkasmani <sarfarazkasmani@gmail.com>
2019-11-12 15:21:32 +0530

    Move out styles in seperate folder

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,37 +1,7 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import MiniPalette from './MiniPalette';
 import { withStyles } from '@material-ui/core/styles';
-
-const styles = {
-	root: {
-		backgroundColor: 'blue',
-		height:'100vh',
-		display:'flex',
-		algnItems: 'flex-start',
-		justifyContent: 'center'
-	},
-	container: {
-		width:'50%',
-		display:'flex',
-		alignItems:'flex-start',
-		flexDirection:'column',
-		flexWrap: 'wrap'
-	},
-	nav: {
-		dipaly:'flex',
-		width:'100%',
-		justifyContent:'space-between',
-		color:'white'
-	},
-	palettes: {
-		boxSizing:'border-box',
-		width:'100%',
-		display:'grid',
-		gridTemplateColumns: 'repeat(3, 30%)',
-		gridGap: '5%'
-	}
-};
+import styles from './styles/PaletteListStyles'
 
 class PaletteList extends Component {
 	
